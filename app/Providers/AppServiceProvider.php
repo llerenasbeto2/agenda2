5f4540edf5896b0186665a81ad2fa41b9391e2d9
jesus <jllerenas1@ucol.mx>
2025-09-13 16:38:58 -0600

    provider actualizado

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
-        if ($this->app->environment('production')) {
+          if (config('app.env') !== 'local') {
             URL::forceScheme('https');
         }
 
